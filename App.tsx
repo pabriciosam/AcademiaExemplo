5127dd8749f00d14cc3361c340ddf7a09deda732
Pabrício <pabricio.samagaio@gmail.com>
2024-07-22 10:52:59 -0300

    Criação de temas de cores

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
-import { Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
 import { Loading } from '@components/Loading';
+import { THEME } from 'src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   
   return (
-    <GluestackUIProvider config={config}>
+    <GluestackUIProvider config={config} globalStyles={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       
-      {!fontsLoaded ? <Text>Cheguei!!!</Text> : <Loading/>}
+      {fontsLoaded ? <View/> : <Loading/>}
     </GluestackUIProvider>
   );
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,10 @@
 import { Center, Spinner } from "@gluestack-ui/themed";
+import { THEME } from 'src/theme';
 
 export function Loading(){
   return(
-    <Center flex={1} bg="$blue800">
-      <Spinner></Spinner>
+    <Center flex={1} bg='$trueGray700'>
+      <Spinner color={THEME.colors.green[500]}/>
     </Center>
   );
 }
